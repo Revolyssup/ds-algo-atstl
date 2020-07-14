7a2c0809dfecec6909797a6c972aee72b7ff85e2
Ashish <ashishjaitiwari15112000@gmail.com>
2020-07-14 22:24:41 +0530

    included Foreach

--- atstllib/test.cpp
+++ atstllib/test.cpp
@@ -2,15 +2,20 @@
 #include<vector>
 #include "atstl.hpp"
 
+//takes pointer to a node and doubles data of all nodes
 void twice(atstl::node<int>* n){
     n->data=(n->data)*2;
 }
 
 int main(){
-    void(*cb)(atstl::node<int>*)=twice;
     std::vector<int> vec={12,58,96,23,1,4};
+    //creates list from vector
     atstl::LinkedList<int> list(vec);
+
+    //prints data of all nodes
     list.print();
-    list.Foreach(cb);
+    //twice passed as a callback(twice is implictly converted into a function pointer so we could have explicitly use &twice)
+    list.Foreach(twice);
+    std::cout<<"*******************\n";
     list.print();
 }
